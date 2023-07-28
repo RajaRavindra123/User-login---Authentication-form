38f564a92c497fa4d9d2f0505dda7801b53ebac1
Raja Ravindra <rajaravindra.marolix@gmail.com>
2023-07-28 07:54:06 +0530

    commiting files

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -17,6 +17,7 @@ def home(request):
     return render(request,"myapp/home.html")
 
 def signup(request):
+    
     if request.method == "POST":
         username = request.POST['username']
         fname = request.POST['fname']
@@ -24,8 +25,7 @@ def signup(request):
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
-
-
+        
         if User.objects.filter(username=username):
             messages.error(request, "Username already exist! Please try some other username.")
             return redirect('home')
@@ -46,15 +46,14 @@ def signup(request):
             messages.error(request, "Username must be Alpha-Numeric!!")
             return redirect('home')
           
-
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
         # myuser.is_active = False
         myuser.is_active = False
         myuser.save()
-
-        messages.success(request, "Your Account has been created successfully.Please check your email to confirm your email address in order to activate your account.")
+        messages.success(request, "Your Account has been created successfully!!! Please check your email to confirm your email address in order to activate your account.")
+        
         # Welcome Email
         subject = "Welcome to Ravindra - Django Login!!"
         message = "Hello " + myuser.first_name + "!! \n" + "Welcome to Ravindra!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"        
@@ -64,7 +63,7 @@ def signup(request):
         
         # Email Address Confirmation Email
         current_site = get_current_site(request)
-        email_subject = "Confirm your Email @Ravindra - Django Login!!"
+        email_subject = "Confirm your Email @ GFG - Django Login!!"
         message2 = render_to_string('email_confirmation.html',{
             
             'name': myuser.first_name,
@@ -80,9 +79,11 @@ def signup(request):
         )
         email.fail_silently = True
         email.send()
-        return redirect('signin')
+        
+        return redirect('signup')
+        
+    return render(request,"myapp/signup.html")
 
-    return render(request,"myapp/signin.html")
 def activate(request,uidb64,token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
@@ -100,22 +101,25 @@ def activate(request,uidb64,token):
     else:
         return render(request,'activation_failed.html')
 
-def signin(request):
 
+def signin(request):
+    
     if request.method == 'POST':
         username = request.POST['username']
         pass1 = request.POST['pass1']
         
         user = authenticate(username=username, password=pass1)
-
+        
         if user is not None:
-            signin(request, user)
+            login(request, user)
             fname = user.first_name
-            return render(request, "authentication/index.html",{"fname":fname})
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "myapp/index.html",{"fname":fname})
         else:
-            messages.error(request, "You Successfully Logged In!!")
+            messages.error(request, "Bad Credentials!!")
             return redirect('home')
-    return render(request,"myapp/signin.html")
+        
+    return render(request, "myapp/signin.html")
 
 def signout(request):
     logout(request)
